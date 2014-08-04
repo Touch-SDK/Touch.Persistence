a7b877c8bd78f2d6ebafa8e4ca548eb4619b754f
Artem Abashev <artem@abashev.com>
2014-08-04 16:55:59 +0400

    PostgreSQL convention.

---
+++ Touch.Domain.NHibernate/Conventions/PostgreSqlConvention.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Inspections;
+using FluentNHibernate.Conventions.Instances;
+
+namespace Touch.Domain.Conventions
+{
+    /// <summary>
+    /// PostgreSQL mapping convention.
+    /// </summary>
+    sealed public class PostgreSqlConvention : IPropertyConvention
+    {
+        public void Apply(IPropertyInstance instance)
+        {
+            if (instance.Type == typeof(string) && ((IPropertyInspector)instance).Length > 255)
+            {
+                instance.CustomSqlType("text");
+            }
+        }
+    }
+}
